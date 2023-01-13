53adb2192609544e7ed042d72b7102c0ca0edf6e
Alucard <85690382+Alucard2169@users.noreply.github.com>
2023-01-13 15:40:59 +0530

    add middleware

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const app = express();
 const PORT = 8000 || process.env.PORT;
 require('dotenv').config();
 const axios = require('axios')
+const path = require('path')
+
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.static(path.join(__dirname+'/public')))
+
+
 
 app.get('/api/:ip', async (req, res) => {
     const ipAddress = req.params.ip
